dbbfeae242eb3daa510b06ea4eeaa2749311d2ea
Zack Hartmann <zackhartmann@gmail.com>
2017-07-13 19:11:18 -0400

    Set script to use static .json for debugging purposes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-// var url_query_local = "https://api.wunderground.com/api/fd97edca66614862/conditions/q/RI/Providence.json"
+// var url_query_local = "https://api.wunderground.com/api/fd97edca66614862/conditions/geolookup/q/41.828855,-71.43304959999999.json";
 
-var api_key = "fd97edca66614862"
-
+//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//
+// Geolocal Coordinates grab & construct JSON url for wunderground.
+//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//
+var api_key = "fd97edca66614862";
 
 var options = {
   enableHighAccuracy: true,
@@ -10,8 +12,10 @@ var options = {
 };
 
 function success(pos) {
-    var url_query_local = 'https://api.wunderground.com/api/' + api_key + '/conditions/geolookup/q/' + pos.coords.latitude + ',' + pos.coords.longitude + '.json';
-    // var url_query_local = "https://api.wunderground.com/api/fd97edca66614862/conditions/q/RI/Providence.json";
+    url_query_local = 'https://api.wunderground.com/api/' +
+                      api_key + '/conditions/geolookup/q/' +
+                      pos.coords.latitude + ',' + pos.coords.longitude + '.json';
+
     console.log(url_query_local);
 };
 
@@ -23,17 +27,33 @@ navigator.geolocation.getCurrentPosition(success, error, options);
 
 
 
+//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//
+// AJAX for WUNDERGROUND.com weather API
+//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//
 $.ajax({
-  url: "https://api.wunderground.com/api/fd97edca66614862/conditions/geolookup/q/41.8288517,-71.433088.json",
+  // url: url_query_local,
+  url: "https://api.wunderground.com/api/fd97edca66614862/conditions/geolookup/q/41.828798,-71.4336524.json",
   dataType: "json",
   success: function(url) {
     console.log(url);
     var location = url.current_observation.display_location.full;
     var temp_f = url.current_observation.temp_f;
-    $(".conditions").html("Current temperature in " + location + " is: " + temp_f + "ºF");
+    // $(".conditions").html("Current temperature in " + location + " is: " + temp_f + "ºF");
+    $(".locale").html(location);
+    $("#currentconditions").html(url.current_observation.weather);
+    // $(".weatherdate").html(url.)
+    // $(".icon_url")
+    // $(".forecast_url")
   }
 });
 
+
+
+
+
+//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//
+// jquery for WUNDERGROUND.com weather API
+//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//
 // $(document).ready(function() {
 //   $.getJSON(success.url_query_local, function(response) {
 //     var location = response['current_observation']['display_location']['city'];
